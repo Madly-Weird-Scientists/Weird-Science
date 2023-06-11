d10bc7e30b4e4ebdc25be2fb8cdf97def806a807
pouffy <pouffygamer@outlook.com>
2023-06-11 16:23:54 +0100

    Added functionality to the Scientists Tablet for Patchouli!

--- src/main/java/com/mad_scientists/weird_science/item/ScientistsTablet.java
+++ src/main/java/com/mad_scientists/weird_science/item/ScientistsTablet.java
@@ -1,9 +1,20 @@
 package com.mad_scientists.weird_science.item;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.registries.ForgeRegistries;
+import vazkii.patchouli.api.PatchouliAPI;
 
+import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Objects;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
@@ -12,14 +23,14 @@ public class ScientistsTablet extends Item {
         super(properties.stacksTo(1));
     }
 
-    //@Nonnull
-    //@Override
-    //public InteractionResultHolder<ItemStack> use(Level worldIn, Player playerIn, InteractionHand handIn) {
-    //    ItemStack stack = playerIn.getItemInHand(handIn);
-    //    if(playerIn instanceof ServerPlayer serverPlayer) {
-    //        PatchouliAPI.get().openBookGUI(serverPlayer, Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(this)));
-    //    }
-//
-    //    return new InteractionResultHolder<>(InteractionResult.CONSUME, stack);
-    //}
+    @Nonnull
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level worldIn, Player playerIn, InteractionHand handIn) {
+        ItemStack stack = playerIn.getItemInHand(handIn);
+        if(playerIn instanceof ServerPlayer serverPlayer) {
+            PatchouliAPI.get().openBookGUI(serverPlayer, Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(this)));
+        }
+
+        return new InteractionResultHolder<>(InteractionResult.CONSUME, stack);
+    }
 }
